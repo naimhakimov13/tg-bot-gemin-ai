b897a85053b87c69b4f3935ac01bf801a451e418
Hakimov <naim.nanocoding@gmail.com>
2024-03-14 12:34:50 +0500

    add express

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import { Telegraf } from 'telegraf';
+import express from 'express'
 
 import 'dotenv/config';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
+const app = express()
 bot.command('oldschool', (ctx) => ctx.reply('Hello'));
 bot.command('hipster', (ctx) => ctx.reply('Feeling those vintage vibes ')); // More descriptive hipster response
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+app.get('/', (req, res, next) => {
+  res.send('telegram bot')
+})
+app.listen(3000, () => {
+  console.log('Server started');
+})
 
 if (process.env.NODE_ENV === "production") {
   bot.launch({
