93fed7a156812ce5fe88e621f25691d8571b7539
Hakimov <naim.nanocoding@gmail.com>
2024-03-14 12:22:02 +0500

    add express

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,22 @@
 import { Telegraf } from 'telegraf';
-import express from "express";
 
 import 'dotenv/config';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.command('oldschool', (ctx) => ctx.reply('Hello'));
-bot.command('hipster', Telegraf.reply('λ'));
+bot.command('hipster', (ctx) => ctx.reply('Feeling those vintage vibes ')); // More descriptive hipster response
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(bot.createWebhook({ domain: 'https://average-pike-long-underwear.cyclic.app' }));
-
-  const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
+  bot.launch({
+    webhook:{
+      domain: process.env.TELEGRAM_WEBHOOK_DOMAIN,
+      port: 3000
+    }
   });
 } else {
   // Use Long Polling for development
